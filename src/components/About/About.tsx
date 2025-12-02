c9af2ec095bd4c36635508cad8ce6936a27e9877
MrCurious-glitch <subhanshc@gmail.com>
2025-12-03 02:38:12 +0530

    commit Add ecommerce app frontend project details

---
+++ src/components/About/About.tsx
@@ -0,0 +1,97 @@
+import { Container } from "./styles";
+// import SaurabhSanap from "../../assets/SaurabhSanap.png";
+import wordpress from "../../assets/wordpress.svg";
+import shopify from "../../assets/shopify.svg";
+import jsIcon from "../../assets/js-icon.svg";
+import nodeIcon from "../../assets/node-icon.svg";
+import reactIcon from "../../assets/react-icon.svg";
+import typescriptIcon from "../../assets/typescript-icon.svg";
+import AWS from "../../assets/AWS.svg";
+import Graphql from "../../assets/graphql.svg";
+import Postgresql from "../../assets/postgresql.svg";
+import MongoDB from "../../assets/mongodb.svg";
+import Nextjs from "../../assets/nextjs.svg";
+import Firebase from "../../assets/firebase.svg";
+import boostrapIcon from "../../assets/bootstrap-icon.svg";
+import ScrollAnimation from "react-animate-on-scroll";
+import sqlicon from "../../assets/sql.png";
+import tailwind from "../../assets/tailwindcss.png";
+
+export function About() {
+  return (
+    <Container id="about">
+      <div className="about-text">
+        <ScrollAnimation animateIn="fadeInLeft">
+          <h2>About me</h2>
+        </ScrollAnimation>
+        
+        <ScrollAnimation animateIn="fadeInLeft" delay={0.2 * 1000} style={{ marginTop: "2rem", marginBottom: "2rem" }}>
+        <p> <b>Hello, I'm Subhansh!</b> <br /> Software Developer specialising in JavaScript, ReactJS, with proficiency in emerging technologies such as AWS. Demonstrates strong capabilities in system design, problem-solving, and is experienced in creating reusable components, managing state, optimising performance, and delivering pixel-perfect, user-focused interfaces. Adept at integrating cutting-edge technologies to enhance development and deliver advanced applications.</p>
+
+        </ScrollAnimation>
+        <div className="hard-skills">
+        <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.13 * 1000}>
+              <img src={reactIcon} alt="React" />
+            </ScrollAnimation>
+          </div>
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.16 * 1000}>
+              <img src={nodeIcon} alt="Node" />
+            </ScrollAnimation>
+          </div>
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.14 * 1000}>
+              <img src={typescriptIcon} alt="Typescript" />
+            </ScrollAnimation>
+          </div>
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.17 * 1000}>
+              <img src={AWS} alt="Html" />
+            </ScrollAnimation>
+          </div>
+          
+          
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.18 * 1000}>
+              <img src={Nextjs} alt="Css" />
+            </ScrollAnimation>
+          </div>
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.18 * 1000}>
+              <img src={tailwind} alt="Css" />
+            </ScrollAnimation>
+          </div>
+          
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.17 * 1000}>
+              <img src={sqlicon} alt="Html" />
+            </ScrollAnimation>
+          </div>
+          
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.19 * 1000}>
+              <img src={boostrapIcon} alt="bootstrap" />
+            </ScrollAnimation>
+          </div>
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.19 * 1000}>
+              <img src={jsIcon} alt="JavaScript" />
+            </ScrollAnimation>
+          </div>
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.10 * 1000}>
+              <img src={wordpress} alt="Wordpress" />
+            </ScrollAnimation>
+          </div>
+          
+        </div>
+      </div>
+      {/* <div className="about-image">
+        <ScrollAnimation animateIn="fadeInRight" delay={0.20 * 1000}>
+          <img src={SaurabhSanap} alt="Saurabh Sanap" />
+        </ScrollAnimation>
+      </div> */}
+    </Container>
+  )
+}
