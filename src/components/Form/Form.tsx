d2d3e9e7540d1691f0f1ebbe6418f6d42ee36075
MrCurious-glitch <subhanshc@gmail.com>
2025-12-03 02:55:10 +0530

    Update form submission key for improved functionality

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -6,7 +6,7 @@ import { useEffect, useState } from 'react'
 import validator from 'validator'
 
 export function Form() {
-  const [state, handleSubmit] = useForm('mqkrwvle')
+  const [state, handleSubmit] = useForm('xblnypev')
   const [validEmail, setValidEmail] = useState(false)
   const [isHuman, setIsHuman] = useState(false)
   const [message, setMessage] = useState('')
